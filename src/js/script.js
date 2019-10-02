550a17f24f04d97e23b8a6916c552ac098b67a3f
friederjanmoerman <hijbrid@gmail.com>
2019-10-03 01:40:59 +0200

    added menu toggle functionality and hovering over the categories has a styling now

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+var menuButtons = document.getElementsByClassName('js-toggle-menu');
+var menuToggle = document.getElementsByTagName('body')[0];
+for (var i = 0; i < menuButtons.length; i++) {
+  menuButtons[i].addEventListener('click', function() {
+    if (menuToggle.classList.contains('open-menu')) {
+      menuToggle.classList.remove('open-menu');
+    } else {
+      menuToggle.classList.add('open-menu');
+    }
+  });
+}
