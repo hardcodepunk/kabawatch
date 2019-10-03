0a438a91524e247445b3f102b4f916f9c1bde7e2
friederjanmoerman <hijbrid@gmail.com>
2019-10-03 11:14:38 +0200

    added empty advanced filter overlay

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,23 @@
-var menuButtons = document.getElementsByClassName('js-toggle-menu');
-var menuToggle = document.getElementsByTagName('body')[0];
+var menuToggle = document.querySelector('.body');
+
+var menuButtons = document.querySelectorAll('.js-toggle-menu');
 for (var i = 0; i < menuButtons.length; i++) {
   menuButtons[i].addEventListener('click', function() {
     if (menuToggle.classList.contains('open-menu')) {
       menuToggle.classList.remove('open-menu');
     } else {
       menuToggle.classList.add('open-menu');
+    }
+  });
+}
+
+var advancedFilterButtons = document.querySelectorAll('.js-toggle-advanced-filter');
+for (var i = 0; i < advancedFilterButtons.length; i++) {
+  advancedFilterButtons[i].addEventListener('click', function() {
+    if (menuToggle.classList.contains('open-advanced-filter')) {
+      menuToggle.classList.remove('open-advanced-filter');
+    } else {
+      menuToggle.classList.add('open-advanced-filter');
     }
   });
 }
