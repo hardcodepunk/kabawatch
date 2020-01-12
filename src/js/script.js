b63c30efc764ae46f8c5e931a37f0265268aa0f6
friederjanmoerman <hijbrid@gmail.com>
2020-01-13 00:55:23 +0100

    controls for mobile nav gallery shop item works, just need to build the counter of the pagination

--- src/js/script.js
+++ src/js/script.js
@@ -49,3 +49,82 @@ window.addEventListener("resize", function(){
     shopPreviewImgs[i].style.height = shopListItemWidth;
   }
 });
+
+/*
+
+click right button
+  search active item retrieve img after and retrieve src to display
+  if last item
+    hide button to right
+  if left button hidden
+    show
+
+click left button
+  search active item retrieve img before and retrieve src to display
+  if first item
+    hide button to left
+  if right button hidden
+    show
+
+*/
+
+var galleryBtns = document.querySelectorAll('.shop-item__gallery-controls__btn');
+var btnPrevious = document.getElementById('gallery__btn-previous');
+var btnNext = document.getElementById('gallery__btn-next');
+
+for (var i = 0; i < galleryBtns.length; i++) {
+  galleryBtns[i].addEventListener('click', function() {
+
+    console.log("btn clicked");
+    var displayedItem = document.querySelector('.shop-item__gallery__item.is-being-displayed');
+    console.log(displayedItem);
+
+    // prev btn clicked
+    if (this == btnPrevious) {
+      console.log("previous btn clicked");
+
+      var previousItem = displayedItem.previousElementSibling;
+
+      displayedItem.classList.remove('is-being-displayed');
+      previousItem.classList.add('is-being-displayed');
+      console.log(sliderProjection.src);
+
+      sliderProjection.src = previousItem.querySelector('.shop-item__gallery__item__visual__img').src;
+      console.log(sliderProjection.src);
+
+      console.log(displayedItem);
+      console.log(previousItem);
+
+      if ( previousItem.previousElementSibling == null ) {
+        btnPrevious.classList.add('is-hidden');
+      }
+
+      if (btnNext.classList.contains('is-hidden')) {
+        btnNext.classList.remove('is-hidden');
+      }
+
+    // next btn clicked
+    } else if ( this == btnNext ) {
+      console.log("next btn clicked");
+
+      var nextItem = displayedItem.nextElementSibling;
+
+      displayedItem.classList.remove('is-being-displayed');
+      nextItem.classList.add('is-being-displayed');
+      console.log(sliderProjection.src);
+      sliderProjection.src = nextItem.querySelector('.shop-item__gallery__item__visual__img').src;
+      console.log(sliderProjection.src);
+
+      console.log(displayedItem);
+      console.log(nextItem);
+
+      if ( nextItem.nextElementSibling == null ) {
+        btnNext.classList.add('is-hidden');
+      }
+
+      if (btnPrevious.classList.contains('is-hidden')) {
+        btnPrevious.classList.remove('is-hidden');
+      }
+    }
+  });
+}
